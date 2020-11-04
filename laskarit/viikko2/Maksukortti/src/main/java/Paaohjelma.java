1656904a317994fb050fcbbe91653bc4d51c6f1c
JuusoVe <juuso.vesanto@gmail.com>
2020-11-04 12:27:55 +0200

    initial commit for test practice files

---
+++ laskarit/viikko2/Maksukortti/src/main/java/Paaohjelma.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juuso
+ */
+public class Paaohjelma {
+    public static void main(String[] args) {
+        Maksukortti kortti = new Maksukortti(50);
+        System.out.println(kortti);
+    }
+}
